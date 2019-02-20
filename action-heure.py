1a7da90c0f857d01f885663b206b60197fbc070e
Singerdim <9867460+Singerdim@users.noreply.github.com>
2019-02-20 15:36:18 +0100

    Update action-heure.py

--- action-heure.py
+++ action-heure.py
@@ -43,9 +43,9 @@ def intent_received(hermes, intent_message):
 	print(intent_message.intent.intent_name)
 	print ()
 
-	if intent_message.intent.intent_name == 'Joseph:askTime':
+	if intent_message.intent.intent_name == 'singerdim:askTime':
 
-		sentence = 'Achete toi une montre! Il est '
+		sentence = 'Achete toi une belle montre! Il est '
 		print(intent_message.intent.intent_name)
 
 		now = datetime.now(timezone('Europe/Paris'))
